e9baa9b0c1a8a8d6966a1c194fd4e67b006e8c7f
dearzoe <2421113739@qq.com>
2016-09-21 17:03:39 +0800

    0921

--- ruleCodeForm.js
+++ ruleCodeForm.js
@@ -1,171 +1,273 @@
 /**
- * Created by huwenzhe on 2016/9/14.
+ * Created by U10K on 2016/9/20.
  */
+
+//真实数据
+var parentData={
+    "MAIN": {
+        "EAF_ID": "38D2D1829CEB35B1827D36C6186A157A",
+        "EAF_WHENPUBLISH": "1",
+        "EAF_WAY": "0",
+        "EAF_SAMPLE": "",
+        "EAF_ATTRID": "DC82E7BFA34449DDFAF02AB8B56D286D",
+        "EAF_NAME": "测试规则1",
+        "EAF_ISMAIN": "1"
+    },
+    "SECTIONS": [
+        {
+            "EAF_ID": "038ED95751D23E96FA3756356B36AD0F",
+            "EAF_NAME": "固定字符串编码",
+            "EAF_TYPE": "0",
+            "EAF_MID": "38D2D1829CEB35B1827D36C6186A157A",
+            "EAF_ORDER": 1,
+            "EAF_CONTENT": "LMZ_"
+        },
+        {
+            "EAF_ID": "E40161BC5F5AEF925C8E34ECE1AA6483",
+            "EAF_NAME": "流水编码段1",
+            "EAF_TYPE": "1",
+            "EAF_MID": "38D2D1829CEB35B1827D36C6186A157A",
+            "EAF_ORDER": 2,
+            "EAF_CONTENT": "A87529E7375DC2ADA118B4F82BCEF965"
+        },
+        {
+            "EAF_ID": "3C1938ABE7B233654F95B51219FC40E8",
+            "EAF_NAME": "分隔符",
+            "EAF_TYPE": "0",
+            "EAF_MID": "38D2D1829CEB35B1827D36C6186A157A",
+            "EAF_ORDER": 3,
+            "EAF_CONTENT": "_"
+        },
+        {
+            "EAF_ID": "BA657F08875DA9687B7C57A9A3059A96",
+            "EAF_NAME": "引用属性",
+            "EAF_TYPE": "2",
+            "EAF_MID": "38D2D1829CEB35B1827D36C6186A157A",
+            "EAF_ORDER": 4,
+            "EAF_CONTENT": "A9C922B5FBD91067BDF56FD469E7DFB9"
+        }
+    ],
+    "SNS": [
+        {
+            "EAF_ID": "A87529E7375DC2ADA118B4F82BCEF965",
+            "EAF_STEP": 2,
+            "EAF_INIT": 1,
+            "EAF_LENGTH": 5,
+            "EAF_LAST": 100,
+        }
+    ]
+}
+
+/*****************************************/
+//构造新的表格数据
+var dataDgDataNew = [];
+//这里构造form表单上半段的数据；
+dataDgDataNew["MAIN"]={};
+dataDgDataNew["SECTIONS"]=[];
+dataDgDataNew["SNS"]=[];
+
+//删除数据
+var dataDgDelete=[];
+
+/*****************************************/
 //类型的下拉框数据
 var products = [
-    {productid:'0',name:'固定字符串'},
-    {productid:'1',name:'流水'},
-    {productid:'2',name:'引用属性'}
+    {productid: '0', name: '固定字符串'},
+    {productid: '1', name: '流水'},
+    {productid: '2', name: '引用属性'}
 ];
 //类型为引用属性时的下拉框数据
-var productsAttr=[
-    {id:'0',text:'EAF_ID'},
-    {id:'1',text:'EAF_NAME'},
-    {id:'2',text:'EAF_HEIGHT'},
-    {id:'3',text:'EAF_WIDTH'},
-    {id:'4',text:'EAF_COLOR'}
+var productsAttr = [
+    {id: '0', text: 'EAF_ID'},
+    {id: '1', text: 'EAF_NAME'},
+    {id: '2', text: 'EAF_HEIGHT'},
+    {id: '3', text: 'EAF_WIDTH'},
+    {id: '4', text: 'EAF_COLOR'}
 ]
 //类型为引用属性时的下拉框数据
-var selcombodata={};
+//var selcombodata = {};
 //表格数据
-var dataDgData=[
-    {EAF_NAME:'编码1',EAF_TYPE:"固定字符串",EAF_SORT:"1",EAF_CONTENT:""},
-    {EAF_NAME:'编码2',EAF_TYPE:"流水",EAF_SORT:"7",EAF_CONTENT:""},
-    {EAF_NAME:'编码3',EAF_TYPE:"引用属性",EAF_SORT:"3",EAF_CONTENT:""}
-];
-//表格样式
-var dataGridColumn=[[
-    {field:'EAF_NAME',title:'名称',width:136,align:'center',editor:'text',},
-    {field:'EAF_TYPE',title:'类型',width:143,align:'center',formatter:productFormatter,editor:{type:'combobox',
-        options:{
-            valueField:'productid',
-            textField:'name',
-            data:products,
-            onSelect:seleCom,
-            panelHeight:62
-        }}},
-    {field:'EAF_SORT',title:'排序',width:143,align:'center',editor:{type:'numberbox',
-        options:{
-            required:true
+var dataDgData = [];
+function getDgData(parentData){
+    var obj={};
+    for(var i=0;i<parentData["SECTIONS"].length;i++){
+        for(var key in parentData["SECTIONS"][i]){
+            obj[key]=parentData["SECTIONS"][i][key]
         }
-    },},
-    {field:'EAF_CONTENT',title:'内容',width:143,align:'center',formatter:productsAttrFormatter,editor:{type:'text'}}
+        dataDgData.push(obj)
+        obj={};
+    }
+    return dataDgData
+}
+getDgData(parentData);
+
+
+
+
+//表格样式
+var dataGridColumn = [[
+    {field: 'EAF_NAME', title: '名称', width: 136, align: 'center', editor: 'text',},
+    {field: 'EAF_TYPE', title: '类型', width: 143, align: 'center', formatter: productFormatter, editor: {
+        type: 'combobox',
+        options: {
+            valueField: 'productid',
+            textField: 'name',
+            data: products,
+            onChange:onChangeHandeler,
+            panelHeight: 62
+        }
+    }
+    },
+    {field: 'EAF_ORDER', title: '排序', width: 143, align: 'center', editor: {
+        type: 'numberbox'
+    }
+    },
+    {field: 'EAF_CONTENT', title: '内容', width: 143, align: 'center', formatter: productsAttrFormatter, editor: {
+        type: 'text'
+    }
+    }
 ]];
-function productFormatter(value){
-    for(var i=0; i<products.length; i++){
+/**
+ * “动态下拉框”改变的时候触发
+ * @param newValue   改变后动态下拉框的数据ID
+ * @param oldValue   改变前动态下拉框的数据ID
+ */
+function onChangeHandeler(newValue,oldValue) {
+    //初次点击时候，oldValue为空，直接返回
+    if(oldValue==''){
+        return;
+    }
+    //获取“动态下拉框”改变前的行数据
+    var selrow = $('#ruleGrid').datagrid('getSelected');
+    //行的索引
+    var selrowindex = $('#ruleGrid').datagrid('getRowIndex', selrow);
+    $('#ruleGrid').datagrid('endEdit',selrowindex);
+    //非初次点击，直接清空动态编辑器框中的值
+    selrow.EAF_CONTENT='';
+    $('#ruleGrid').datagrid('beginEdit',selrowindex);
+}
+function productFormatter(value) {
+    for (var i = 0; i < products.length; i++) {
         if (products[i].productid == value) return products[i].name;
     }
     return value;
 }
-function productsAttrFormatter(value,row,index){
-    if(row.EAF_TYPE==2 || row.EAF_TYPE=="引用属性") {
-        if (selcombodata[row.EAF_ID])value=selcombodata[row.EAF_ID];
+//if (selcombodata[row.EAF_ID])value = selcombodata[row.EAF_ID];
+//渲染“动态编辑框”
+function productsAttrFormatter(value, row, index) {
+    //判断“动态下拉框”为引用属性的时候，把value替换成 productsAttr中对应的值；
+    if (row.EAF_TYPE == 2) {
         for (var i = 0; i < productsAttr.length; i++) {
             if (productsAttr[i].id == value) return productsAttr[i].text;
         }
-    }else if (row.EAF_TYPE == 1 || row.EAF_TYPE == "流水") {
-        row.EAF_CONTENT = "";
-    } else if (row.EAF_TYPE == 0 || row.EAF_TYPE == "固定字符串") {
-        return value;
     }
+    return value;
 }
-function seleCom(p){
-    debugger;//1
+function seleCom(p) {
     var selrow = $('#ruleGrid').datagrid('getSelected');
-    var selrowindex = $('#ruleGrid').datagrid('getRowIndex',selrow);
-    $('#ruleGrid').datagrid('endEdit', selrowindex);
-    $('#ruleGrid').datagrid('beginEdit', selrowindex);
-   /* var ed = $('#ruleGrid').datagrid('getEditor', {index:selrowindex,field:'EAF_CONTENT'});
-    $.parser.parse();
-    if(p.productid == 0){
-        $(ed.target).textbox({
-            height:22,
-            width:142,
-            icons:[{
-                iconCls:'icon-edit'
-            }],
-            iconWidth:0
-        })
-        $(ed.target).textbox('reset')
-    }else if(p.productid == 1){
-        $(ed.target).textbox({
-            height:30,
-            width:142,
-            icons:[{
-                iconCls:'icon-edit',
-                handler: openDialog,
-                iconAlign:"right"
-            }],
-            height:20,
-            iconWidth:135
-        })
-        $(ed.target).textbox('reset')
-    }else if(p.productid == 2){
-        debugger;//1
-        var row = $('#ruleGrid').datagrid('getSelected');
-        row.EAF_CONTENT="";
-        $(ed.target).combobox({
-            height:22,
-            width:142,
-            valueField:'id',
-            textField:'text',
-            data:productsAttr,
-            onSelect: function(rec){
-                selcombodata[selrow.EAF_ID]=rec.id;
-                selcombodata[selrow.EAF_CONTENT]=rec.text
-            },
-            panelHeight:0
-        });
-        $(ed.target).combobox('setValue',selcombodata[selrow.EAF_CONTENT])
-        if(selcombodata[selrow.EAF_ID])  $(ed.target).combobox('select',selcombodata[selrow.EAF_ID]);
-    }
-    $.parser.parse();*/
+    var selrowindex = $('#ruleGrid').datagrid('getRowIndex', selrow);
+    $('#ruleGrid').datagrid('endEdit',selrowindex);
+    $('#ruleGrid').datagrid('beginEdit',selrowindex);
 }
-/*function context(b){  //formatter:context,放在第四列
- var ed = $('#ruleGrid').datagrid('getEditor',{index:0,field:'EAF_CONTENT'});
- }*/
-$("#dg1").dialog({
-    title:"流水",
-    width:300,
-    height:200,
-    buttons:[
+$("#waterGrid").dialog({
+    title: "流水",
+    width: 300,
+    height: 200,
+    buttons: [
         {
-            text:"重置",
-            plain:true,
-            iconCls:"icon-reload",
-            handler:function(){
+            text: "重置",
+            plain: true,
+            iconCls: "icon-reload",
+            handler: function () {
                 alert("我点击了流水的reset键！")
             }
         },
         {
-            text:"确定",
-            plain:true,
-            iconCls:"icon-ok",
-            handler:function(){
-                alert("我点击了流水的OK键！")
+            text: "确定",
+            plain: true,
+            iconCls: "icon-ok",
+            handler: function () {
+                //保存流水数据
+                var snsObj={};
+                var selrow = $('#ruleGrid').datagrid('getSelected');
+                var selrowId=selrow.EAF_CONTENT
+                if(!selrowId){
+                //赋值一个，这个是我给的假的，所以只能添加一条流水
+                var selrowId="";
+                for(var i=0; i<32;i++){
+                    selrowId+=Math.floor(Math.random()*10)
+                }
+                snsObj.EAF_ID = selrowId;
+                snsObj.EAF_INIT = $("#wgInit").val()
+                snsObj.EAF_LENGTH = $("#wgLength").val()
+                snsObj.EAF_STEP = $("#wgStep").val()
+                snsObj.EAF_LAST = $("#wgLast").val()
+                dataDgDataNew["SNS"].push(snsObj);
+                snsObj={}
+                }
+                $("#waterGrid").dialog('close')
             }
-        },{
-            text:"取消",
-            plain:true,
-            iconCls:"icon-cancel",
-            handler:function(){
-                alert("我点击了流水的cancel键！")
+        }, {
+            text: "取消",
+            plain: true,
+            iconCls: "icon-cancel",
+            handler: function () {
+                $("#waterGrid").dialog('close')
             }
         }]
 });
+//打开流水窗口并且加载数据
 function openDialog(){
-    $('#dg1').dialog('open');
-}
-function onBeforeEditHandeler(rowIndex, rowData) {
-    var col = $(this).datagrid('getColumnOption', 'EAF_CONTENT');
-    // 类型
-    var type = $('#ruleGrid').datagrid('getSelected').EAF_TYPE;
-    if (type == '固定字符串' || type == '0') {
-        col.editor.type = 'text';
+    $('#waterGrid').dialog('open');
+    //更新流水数据
+    var selrow = $('#ruleGrid').datagrid('getSelected');
+    var selrowId=selrow.EAF_CONTENT
+    if(selrowId){
+        for(var i=0;i<parentData["SNS"].length;i++){
+            var curId=parentData["SNS"][i].EAF_ID;
+            //如果数据中的流水ID等等于所选行的EAF_CONTENT（流水ID）
+            if(curId == selrowId){
+                $("#wgInit").val(parentData["SNS"][i]["EAF_INIT"])
+                $("#wgLength").val(parentData["SNS"][i]["EAF_LENGTH"])
+                $("#wgStep").val(parentData["SNS"][i]["EAF_STEP"])
+                $("#wgLast").val(parentData["SNS"][i]["EAF_LAST"])
+            }
+        }
     }
-    else if (type == '流水' || type == '1') {
-        col.editor.type = 'textbox';
-        col.editor.options = {
+}
+//关闭流水窗口的时候清空数据
+function closeDialog() {
+    $('#waterGrid').dialog({
+        onClose: function () {
+            $("#wgInit").val("");
+            $("#wgLength").val("");
+            $("#wgStep").val("");
+            $("#wgLast").val("");
+        }
+    });
+}
+closeDialog()
+
+function onBeforeEditHandeler(rowIndex, rowData){
+    var col = $(this).datagrid('getColumnOption','EAF_CONTENT');
+    //类型
+    var type=$('#ruleGrid').datagrid('getSelected').EAF_TYPE;
+    if(type=='0'){
+        col.editor.type='text';
+    }
+    else if(type=='1'){
+        col.editor.type='textbox';
+        col.editor.options= {
             icons: [{
                 iconCls: 'icon-edit',
-                handler: editClick
+                handler: openDialog
             }],
-            iconWidth: 120
+            iconWidth: 138
         };
     }
-    else if (type == '引用属性' || type == '2') {
-        col.editor.type = 'combobox';
-        col.editor.options = {
+    else if(type=='2'){
+        col.editor.type='combobox';
+        col.editor.options={
             valueField: 'id',
             textField: 'text',
             data: productsAttr,
@@ -173,65 +275,77 @@ function onBeforeEditHandeler(rowIndex, rowData) {
         }
     }
 }
-$(function(){
+$(function () {
     var lastIndex;
     $('#ruleGrid').datagrid({
         onBeforeEdit: onBeforeEditHandeler,
-        toolbar:[{
-            text:'添加',
-            iconCls:'icon-add',
-            handler:function(){
+        striped:true,
+        toolbar: [{
+            text: '添加',
+            iconCls: 'icon-add',
+            handler: function(){
                 $('#ruleGrid').datagrid('endEdit', lastIndex);
-                $('#ruleGrid').datagrid('appendRow',{
-                    EAF_NAME:'',
-                    EAF_TYPE:'0',
-                    EAF_SORT:'',
-                    EAF_CONTENT:''
+                $('#ruleGrid').datagrid('appendRow', {
+                    EAF_NAME: '',
+                    EAF_TYPE: '0',
+                    EAF_ORDER: '1',
+                    EAF_CONTENT: ''
                 });
-                lastIndex = $('#ruleGrid').datagrid('getRows').length-1;
+                lastIndex = $('#ruleGrid').datagrid('getRows').length - 1;
                 $('#ruleGrid').datagrid('selectRow', lastIndex);
                 $('#ruleGrid').datagrid('beginEdit', lastIndex);
             }
-        },'-',{
-            text:'删除',
-            iconCls:'icon-remove',
+        }, '-', {
+            text: '删除',
+            iconCls: 'icon-remove',
             handler:function(){
                 var row = $('#ruleGrid').datagrid('getSelected');
-                if (row){
+                dataDgDelete.push(row.EAF_ID)
+                if (row) {
                     var index = $('#ruleGrid').datagrid('getRowIndex', row);
                     $('#ruleGrid').datagrid('deleteRow', index);
                 }
             }
-        },'-',{
-            text:'保存',
-            iconCls:'icon-save',
-            handler:function(){
-                $('#ruleGrid').datagrid('acceptChanges');
-            }
         }],
-        onBeforeLoad:function(){
+        onBeforeLoad: function () {
             $(this).datagrid('rejectChanges');
         },
-        onClickRow:function(rowIndex,row){
-            if(row.type == '固定字符串'){
-               $(ed.target).combobox('setValue',selcombodata[selrow.EAF_CONTENT])
-            }
-            if (lastIndex != rowIndex){
+        onClickRow: function (rowIndex, row) {
+            if (lastIndex != rowIndex) {
                 $('#ruleGrid').datagrid('endEdit', lastIndex);
                 $('#ruleGrid').datagrid('beginEdit', rowIndex);
             }
             lastIndex = rowIndex;
         },
         //表格样式
-        columns:dataGridColumn,
+        columns: dataGridColumn,
         //表格数据
-        data:dataDgData
+        data: dataDgData
     });
     $.parser.parse();
 });
-
-/*
-//返回选择行数据
-function getResult() {
-    return "";
-};*/
+function getResult(){
+    //获取列表中的“规则名称”
+    dataDgDataNew["MAIN"]["EAF_NAME"]=$("#ruleName").val();
+    //获取列表中的“生成方式”
+    dataDgDataNew["MAIN"]["EAF_WAY"]=$("#bornStyle").combobox("getValue");
+    //判断并获取列表中的“是否为主编码”
+    if($("#mainCode").is(':checked')){
+        dataDgDataNew["MAIN"]["EAF_ISMAIN"]=1
+    }else{
+        dataDgDataNew["MAIN"]["EAF_ISMAIN"]=0
+    }
+    //判断并获取列表中的“是否在发布时生成”
+    if($("#bornPublish").is(':checked')){
+        dataDgDataNew["MAIN"]["EAF_WHENPUBLISH"]=1
+    }else{
+        dataDgDataNew["MAIN"]["EAF_WHENPUBLISH"]=0
+    }
+    var selrow = $('#ruleGrid').datagrid('getSelected');
+    debugger;//1
+    var selrowindex = $('#ruleGrid').datagrid('getRowIndex', selrow);
+    $('#ruleGrid').datagrid('endEdit', selrowindex);
+    dataDgDataNew["SECTIONS"]= $('#ruleGrid').datagrid("getChanges");
+    console.log(dataDgDataNew)
+    console.log(dataDgDelete)
+}
--- ruleCodeForm.jsp
+++ ruleCodeForm.jsp
@@ -1,42 +1,49 @@
-<%@ include file='/main/head.jsp' %>
+﻿<%@ include file='/main/head.jsp' %>
 <script src="<%=eafapppath %>/main/UserInterface/control.js" type="text/javascript"></script>
 <script src="<%=eafapppath %>/main/UserInterface/form.js" type="text/javascript"></script>
-    <form id="ruleForm" action="" style="margin:20px 0px 20px 0px">
+    <form id="ruleForm" action="" style="margin:20px 0px 20px 0px;font-size:12px">
     <div style="padding:5px;">
-    <label for="ruleName" style="margin-left:50px;width:110px;display:inline-block">&nbsp;&nbsp;ruleName:</label>
-    <input class="easyui-validatebox" type="text" name="ruleName" style="width:180px">
+    <label for="ruleName" style="margin-left:50px;width:110px;display:inline-block">&nbsp;&nbsp;&nbsp;&nbsp;规则名称：</label>
+    <input id="ruleName" class="easyui-validatebox" type="text" name="ruleName" style="width:180px">
     </div>
     <div style="padding:5px;">
-    <label for="mainCode" style="margin-left:50px;width:110px;display:inline-block">&nbsp;isMainCode:</label>
-    <input class="easyui-validatebox" type="checkbox" name="mainCode">
+    <label for="mainCode" style="margin-left:50px;width:110px;display:inline-block">&nbsp;&nbsp;是否为主编码：</label>
+    <input id="mainCode" type="checkbox" name="mainCode">
     </div>
     <div style="padding:5px;">
-    <label for="mainCode" style="margin-left:50px;width:110px;display:inline-block">&nbsp; bornStyle:</label>
-    <select id="cc" class="easyui-combobox" name="buildAction" style="width:185px;" data-options="panelHeight:42">
-    <option>auto</option>
-    <option>manual</option>
+    <label for="bornStyle" style="margin-left:50px;width:110px;display:inline-block">&nbsp;&nbsp;&nbsp;&nbsp;生成方式：</label>
+    <select id="bornStyle" class="easyui-combobox"  name="bornStyle" style="width:185px;" data-options="panelHeight:42,valueField:'label',
+    textField: 'value',
+    data: [{
+    label: '0',
+    value: '自动',
+    selected:true
+    },{
+    label: '1',
+    value: '手动'
+    }]">
     </select>
     </div>
     <div style="padding:5px;">
-    <label for="bornPublish" style="margin-left:50px;width:110px;display:inline-block">&nbsp;&nbsp;generate:</label>
-    <input class="easyui-validatebox" type="checkbox" name="bornPublish">
+    <label for="bornPublish" style="margin-left:50px;width:110px;display:inline-block">是否在发布时生成：</label>
+    <input id="bornPublish" type="checkbox" name="bornPublish">
     </div>
     <div style="padding:5px;">
-    <label for="example" style="margin-left:50px;width:110px;display:inline-block">&nbsp;codeSample:</label>
-    <input class="easyui-validatebox" type="text" style="width:180px">
+    <label for="example" style="margin-left:50px;width:110px;display:inline-block">&nbsp;&nbsp;&nbsp;&nbsp;编码样例：</label>
+    <input id="example" class="easyui-validatebox" type="text" style="width:180px">
     </div>
     </form>
     <!--上面是input checkbox|| 下面是datadgrid-->
     <div class="easyui-panel" style="width:585px;height:250px;">
-    <table id="ruleGrid" style="width:575px;position:" data-options="singleSelect:true,idField:'itemid'">
+     <table id="ruleGrid" style="width:575px;position:" data-options="singleSelect:true,idField:'itemid'">
     </table>
     </div>
-    <div id="dg1" closed="true">
+    <div id="waterGrid" closed="true">
     <form style="margin: 15px 0px 0px 30px">
-    <div style="margin-top:7px">&nbsp;star:<input type="text" style="text-align:center"></div>
-    <div style="margin-top:7px">length:<input type="text"  style="text-align:center"></div>
-    <div style="margin-top:7px">&nbsp;step:<input type="text"  style="text-align:center"></div>
-    <div style="margin-top:7px">&nbsp; end:<input type="text"  style="text-align:center"></div>
+    <div style="margin-top:7px">&nbsp;初始值：<input id="wgInit" type="text" style="text-align:center"></div>
+    <div style="margin-top:7px">&nbsp;&nbsp;长度：<input id="wgLength" type="text"  style="text-align:center"></div>
+    <div style="margin-top:7px">&nbsp;&nbsp;步长：<input id="wgStep" type="text"  style="text-align:center"></div>
+    <div style="margin-top:7px">最后流水：<input id="wgLast" type="text"  style="text-align:center"></div>
     </form>
     </div>
 <script type="text/javascript" src="ruleCodeForm.js"></script>
